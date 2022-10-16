182193b344bec0cfd470a81154d882be9b5e37ab
BISMILAHONLINE <114799967+BISMILAHONLINE@users.noreply.github.com>
2022-10-17 01:29:22 +0700

    Update setting.js

--- setting.js
+++ setting.js
@@ -10,17 +10,17 @@ global.Api = {
 global.ApiKey = 'coderevolution' //JIKA INGIN MENDAPATKAN KOMISI DAFTAR DI HTTPS://AMPIBISMM.MY.ID UNTUK MENDAPATKAN APIKEY!
 
 global.smm = {
-  nomor : '6283102650464',
-  atasnama : 'Ahmad Nur Fadhil'
+  nomor : '6288980870067',
+  atasnama : 'Swift Developer'
 }
 
 // Other
-global.owner = ['6283102650464',"6282135724604"]
-global.premium = ['6283102650464']
+global.owner = ['6288980870067',"6288980870067"]
+global.premium = ['6288980870067']
 global.sponsor = 'no sponsors in:('
-global.packname = 'ChanZxD-Bot'
-global.footer = '© 2022 IchanZxD-MD'
-global.author = 'Owner : Fadhil Graphy\nIg1 : @fdhlgrphy\nIg2 : @nr.fdhl__'
+global.packname = 'Siesta - Multi Device'
+global.footer = '© 2022 Siesta - Multi Device'
+global.author = 'Owner : Swift Graphy\nIg1 : @swift\nIg2 : @swift'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.act = {
 		wait: 'please wait a moment, processing data...',
